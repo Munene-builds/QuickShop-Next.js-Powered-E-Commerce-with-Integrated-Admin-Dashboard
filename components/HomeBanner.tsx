7c29f451e7ecfb3ddc570492e2566235d467218b
Munene Builds <onlyfansdirectai@gmail.com>
2025-09-30 07:17:57 +0300

    Update: Improved navbar layout and redesigned hero section

---
+++ components/HomeBanner.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {Title} from "./ui/text";
+const HomeBanner = () => {
+	return(
+		 <div className="py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 
+		 	flex items-center justiyf-between">
+
+		 	<div>
+		 		<Title>
+		 			Grab Upto 50% Off on <br/>
+		 			Selected headphones
+		 		</Title>
+		 		
+		 	</div>
+		 	<div></div> 
+
+		 </div>
+
+		)
+};
+export default HomeBanner;
